c58896d2f5435965052337d6ba0a4845358d2c03
iamsuja <suja.hash69@gmail.com>
2020-09-08 19:18:39 +0530

    Initial commit :wq

---
+++ script/forecast.js
@@ -0,0 +1,19 @@
+const key = "1EWfvfNeA6yWUGRRHJd0jK5KST7kGpvq";
+//get weather
+
+const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[0];
+};
+
+//get city information
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[0];
+};
